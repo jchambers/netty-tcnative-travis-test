31b5fea6523da1cd0f248fd82ee78de21b90d4fd
Jon Chambers <jon.chambers@gmail.com>
2020-02-11 13:02:23 -0500

    Added tests for constructing server SSL contexts.

--- src/test/java/com/eatthepath/netty/TcnativeTest.java
+++ src/test/java/com/eatthepath/netty/TcnativeTest.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 
 import javax.net.ssl.SSLException;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.junit.Assert.assertTrue;
 
 public class TcnativeTest {
@@ -33,5 +37,38 @@ public class TcnativeTest {
         }
 
         assertTrue(sslContext.isClient());
+    }
+
+    @Test
+    public void testServerSslContext() throws IOException {
+        try (final InputStream certificateChainInputStream = getClass().getResourceAsStream("server-certs.pem");
+             final InputStream privateKeyPkcs8InputStream = getClass().getResourceAsStream("server-key.pem")) {
+
+            final SslContext sslContext;
+            {
+                final SslProvider sslProvider = SslProvider.OPENSSL;
+
+                final SslContextBuilder sslContextBuilder =
+                        SslContextBuilder.forServer(certificateChainInputStream, privateKeyPkcs8InputStream, null);
+
+                sslContextBuilder.sslProvider(sslProvider)
+                        .ciphers(Http2SecurityUtil.CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
+                        .clientAuth(ClientAuth.OPTIONAL);
+
+                sslContextBuilder.applicationProtocolConfig(new ApplicationProtocolConfig(
+                        ApplicationProtocolConfig.Protocol.ALPN,
+                        ApplicationProtocolConfig.SelectorFailureBehavior.NO_ADVERTISE,
+                        ApplicationProtocolConfig.SelectedListenerFailureBehavior.ACCEPT,
+                        ApplicationProtocolNames.HTTP_2));
+
+                sslContext = sslContextBuilder.build();
+            }
+
+            if (sslContext instanceof ReferenceCounted) {
+                ((ReferenceCounted) sslContext).release();
+            }
+
+            assertTrue(sslContext.isServer());
+        }
     }
 }
